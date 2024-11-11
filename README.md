0702edafb5bde47dec7e21d238baaad0a296f6f4
Patrick Hähnel <p_haehnel@hotmail.de>
2024-11-11 11:09:32 +0100

    add licence, better docstring and better Readme

--- README.md
+++ README.md
@@ -1,5 +1,78 @@
-# Guessing Game
+# Rust Guessing Game
 
-Simple guessing game written in Rust with colored Terminal output.
+Welcome to the Rust Guessing Game! This is a simple command-line game where the player tries to guess a randomly generated number between 1 and 100.
 
-_This project was created while I was learning the programming language Rust, and shows my progress with this language._
+## Features
+
+- Generates a random number between 1 and 100.
+- Prompts the user to guess the number.
+- Provides feedback if the guessed number is too low, too high, or correct.
+- Uses colored output for better readability.
+
+## Getting Started
+
+### Prerequisites
+
+- [Rust](https://www.rust-lang.org/tools/install) (Make sure you have Rust installed on your system)
+
+### Installation
+
+1. Clone the repository:
+
+    ```sh
+    git clone https://github.com/KarnesTH/guessing_game.git
+    cd rust-guessing-game
+    ```
+
+2. Run the game:
+
+    ```sh
+    cargo run
+    ```
+
+## Usage
+
+When you run the game, you will see the following prompt:
+
+```
+Welcome to Rust Guessing Game!
+Please enter a number:
+```
+
+Enter your guess and press Enter. The game will provide feedback on whether your guess is too low, too high, or correct. The game continues until you guess the correct number.
+
+## Example
+
+```
+Welcome to Rust Guessing Game!
+
+Please enter a number:
+50
+Your number: 50
+Your given number is too high!
+
+Please enter a number:
+25
+Your number: 25
+Your given number is too low!
+
+Please enter a number:
+37
+Your number: 37
+Yeah, you're right!
+```
+
+## Dependencies
+
+This project uses the following Rust crates:
+
+- [colored](https://crates.io/crates/colored): For colored terminal output.
+- [rand](https://crates.io/crates/rand): For generating random numbers.
+
+## License
+
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for details.
+
+## Acknowledgments
+
+- Inspired by the Rust programming language documentation and tutorials.
--- src/main.rs
+++ src/main.rs
@@ -3,41 +3,38 @@ use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
 
-/// This is the main function of the Guessing Game.
-/// It initializes a random number between 1 and 100, then enters a loop where it prompts the user to guess the number.
-/// After each guess, it provides feedback whether the guessed number is too high, too low, or correct.
+/// A simple number guessing game implemented in Rust.
+///
+/// This program generates a random number between 1 and 100 and prompts the user to guess the number.
+/// After each guess, the program provides feedback indicating whether the guessed number is too low,
+/// too high, or correct. The game continues until the user guesses the correct number.
 fn main() {
-    println!("Willkommen zum Guessing Game!\n");
+    println!("Welcome to Rust Guessing Game!");
 
-    // Generate a random number between 1 and 100
     let rnd_num = rand::thread_rng().gen_range(1..=100);
 
     loop {
-        println!("Bitte gebe eine Zahl ein: ");
+        println!("Please enter a number: ");
 
-        // Initialize a mutable string to store the user's guess
         let mut guess = String::new();
 
-        // Read the user's input from the standard input
         io::stdin()
             .read_line(&mut guess)
-            .expect("Es konnte die Eingabe nicht gelesen werden.");
+            .expect("Failed to read input.");
 
-        // Parse the user's input into a u32 and handle any parsing errors
         let guessed_num: u32 = match guess.trim().parse() {
             Ok(num) => num,
-            Err(_) => continue, // If parsing fails, skip to the next iteration of the loop
+            Err(_) => continue,
         };
 
-        println!("Deine Zahl: {}", guessed_num);
+        println!("Your number: {}", guessed_num);
 
-        // Compare the guessed number with the random number and provide feedback
         match guessed_num.cmp(&rnd_num) {
-            Ordering::Less => println!("{}", "Die eingegebene Zahl ist zu klein!".red()),
-            Ordering::Greater => println!("{}", "Die eingegebene Zahl ist zu groß!".red()),
+            Ordering::Less => println!("{}", "Your given number is to low!".red()),
+            Ordering::Greater => println!("{}", "Your given number is to high!".red()),
             Ordering::Equal => {
-                println!("{}", "Richtig geraten!".green());
-                break; // If the guessed number is correct, exit the loop
+                println!("{}", "Yeah, your right!".green());
+                break;
             }
         };
     }
